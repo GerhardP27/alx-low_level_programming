61c76f3db2aae2033b4880d5b37e5893e79a2b4a
GerhardP27 <gerhardpillay.gp@gmail.com>
2022-06-07 12:56:08 -0700

    first check

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Description: Prints Holberton, followed by a new line
+* Return: 0
+*/
+
+int main(void)
+{
+	char main[] = "Main";
+
+	int msg;
+
+	for (msg = 0; msg <= 8; msg++)
+
+	{
+		_putchar(holberton[msg]);
+	}
+	_putchar('\n');
+	return (0);
+}
