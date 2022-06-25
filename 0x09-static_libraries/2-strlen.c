90e7c55fd5f6adc21f2e9d6f51f8d51c4170a374
GerhardP27 <gerhardpillay.gp@gmail.com>
2022-06-25 05:09:19 -0700

    libmy.a fix

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function with 1 argument
+ * @s: char type pointer
+ * Description: strlen similiar function to return length
+ * Return: string
+ */
+
+int _strlen(char *s)
+
+{
+int len = 0;
+
+while (*s++)
+	len++;
+
+return (len);
+}
