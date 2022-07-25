4b7d51bd278ada5c06b44b57931b26e5e2eef522
GerhardP27 <gerhardpillay.gp@gmail.com>
2022-07-25 12:16:06 -0700

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -75,8 +75,8 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDER_FILEN,
-				"ErrorRO: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
