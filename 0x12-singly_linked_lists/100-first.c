2cec283c2191b5b3fb13d021679bb7886b1e3e5b
GerhardP27 <gerhardpillay.gp@gmail.com>
2022-09-06 05:05:57 -0700

    test fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void __attribute__((constructor)) hare(void);
 void hare(void)
 {
 	fprintf(stderr, "You're beat! and yet, you must allow,\n"
-			"I bore my house up on my back!\n");
+			"I bore my house up on my back!\n"
+			"(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
